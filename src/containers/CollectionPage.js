00ecb3e22e01c4a5af67a20f16ddeac2e57ec11a
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-05-29 10:56:50 -0500

    added sentiment analyzer response

--- src/containers/CollectionPage.js
+++ src/containers/CollectionPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import QuoteCollection from './QuoteCollection.js'
 
+
 const URL = "http://localhost:3000/quotes"
 
 class CollectionPage extends Component {
@@ -8,11 +9,63 @@ class CollectionPage extends Component {
         super();
         this.state = {
             quotes: [],
+            q: []
         }
     }
 
     componentDidMount() {
         this.getQuotes()
+    }
+
+    positiveReturn() {
+        document.querySelector("body").style.backgroundColor = "#dbce94"
+        document.querySelector("button").style.backgroundColor = "orange"
+
+        alert("Hey, you're pretty positive! Keep up the good work!")
+    }
+
+    negativeReturn() {
+        document.querySelector("body").style.backgroundColor = "maroon"
+        document.querySelector("button").style.backgroundColor = "grey"
+        alert("Hey, you suck. Why not make some better life choices?")
+    }
+
+    neutralReturn() {
+        // document.querySelector("button").style.backgroundColor = "white"
+        alert("meh. who cares")
+    }
+
+    returnSentiment() {
+        if (this.state.q >= 1) {
+           this.positiveReturn()
+        }
+        else if (this.state.q <= -1) {
+            this.negativeReturn()
+        }
+        else {
+            this.neutralReturn()
+        }
+    }
+
+    getAnalysis = (e) => {
+        this.setState({
+            q: this.state.quotes.filter(quote => quote.saved).map(quote => {
+                if (quote.sentiment === "positive") {
+                    return 1
+                }
+                else if (quote.sentiment === "negative") {
+                    return -1
+                }
+                else if (quote.sentiment === "neutral") {
+                    return 0
+                }
+            }
+            ).reduce(function (total, amount) {
+                return total + amount
+            })
+        })
+
+        this.returnSentiment()
     }
 
     getQuotes() {
@@ -25,9 +78,9 @@ class CollectionPage extends Component {
 
     render() {
         return (
-            <div>
+            <div className="body">
                 <QuoteCollection quotes={this.state.quotes.filter(quote => quote.saved)} />
-                
+                <button className="btnnDiv" onClick={(e) => this.getAnalysis()}>Analyze My Sentiments!</button>
             </div>
         )
     }
